92e9e84a8687f3e00275792403293ba15eb0bf14
Aadit B <62301974+abennur33@users.noreply.github.com>
2022-11-07 17:31:50 -0500

    Update StoreFront.java

--- src/StoreFront.java
+++ src/StoreFront.java
@@ -6,5 +6,11 @@ public class StoreFront {
     private String sellerUsername;
     private ArrayList<Product> products;
     private ArrayList<Sale> sales;
-    
+
+    public StoreFront(String storeFrontName, String sellerUsername, ArrayList<Product> products, ArrayList<Sale> sales) {
+        this.storeFrontName = storeFrontName;
+        this.sellerUsername = sellerUsername;
+        this.products = products;
+        this.sales = sales;
+    }
 }
