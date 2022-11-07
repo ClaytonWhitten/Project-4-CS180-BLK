7695e8398a03a504003b8278e8c2c690845d9b9b
Aadit B <62301974+abennur33@users.noreply.github.com>
2022-11-07 18:15:27 -0500

    Created Buyer file reader

--- src/Buyer.java
+++ src/Buyer.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Buyer extends User {
@@ -7,9 +11,54 @@ public class Buyer extends User {
 
     private ArrayList<Sale> purchases;
 
-    public Buyer(String username, String password, String name) {
-        super(username, password);
+    public Buyer(User user, String name) {
+        super(user.getUsername(), user.getPassword(), user.getType());
         this.name = name;
+        shoppingCart = new ArrayList<>();
+        purchases = new ArrayList<>();
+        readBuyerFile(user.getUsername() + ".txt");
     }
 
+    private void readBuyerFile(String fileName) {
+        BufferedReader bfr = null;
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            File f = new File(fileName);
+            FileReader fr = new FileReader(f);
+            bfr = new BufferedReader(fr);
+            String currentLine = bfr.readLine();
+            while (currentLine != null) {
+                lines.add(currentLine);
+                currentLine = bfr.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bfr != null) {
+                try {
+                    bfr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        String[] tempProductFields;
+        String[] tempSalesArray;
+        String[] tempSalesFields;
+
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).charAt(0) == '-') {
+                tempProductFields = lines.get(i).split("::");
+                shoppingCart.add(new Product(tempProductFields[0], tempProductFields[1], tempProductFields[2], Integer.parseInt(tempProductFields[3]), Double.parseDouble(tempProductFields[4])));
+            }
+            if (lines.get(i).charAt(0) == '>') {
+                tempSalesArray = lines.get(i).substring(1).split(";");
+                for (int k = 0; k < tempSalesArray.length; k++) {
+                    tempSalesFields = tempSalesArray[k].split(",");
+                    purchases.add(new Sale(tempSalesFields[0], tempSalesFields[1], Double.parseDouble(tempSalesFields[2])));
+                }
+            }
+        }
+    }
 }
