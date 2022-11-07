f5d8245d8ba89af67b654ede733393beb55f133a
Clayton <claytonwhitten63@gmail.com>
2022-11-07 16:27:05 -0500

    Setup basic Sale objects

---
+++ src/Sale.java
@@ -0,0 +1,11 @@
+public class Sale {
+
+    private String customerInfo;
+    private double revenue;
+
+    public Sale(String customerInfo, double revenue) {
+        this.customerInfo = customerInfo;
+        this.revenue = revenue;
+    }
+
+}
