ed965dc1d4d6a35fdb32750560410f2bec30744f
Aadit B <62301974+abennur33@users.noreply.github.com>
2022-11-07 16:35:28 -0500

    Update Buyer.java

--- src/Buyer.java
+++ src/Buyer.java
@@ -5,6 +5,8 @@ public class Buyer extends User {
     private String name;
     private ArrayList<Product> shoppingCart;
 
+    private ArrayList<Sale> purchases;
+
     public Buyer(String username, String password, String name) {
         super(username, password);
         this.name = name;
