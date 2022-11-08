b948df0004e5f9db38a83a623c61962d290c8a05
Aadit B <62301974+abennur33@users.noreply.github.com>
2022-11-08 00:33:07 -0500

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -14,6 +14,43 @@ public class Product {
         this.price = price;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getStoreFrontName() {
+        return storeFrontName;
+    }
+
+    public void setStoreFrontName(String storeFrontName) {
+        this.storeFrontName = storeFrontName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
