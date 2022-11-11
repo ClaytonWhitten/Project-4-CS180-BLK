25e197daa77e40e9192be43957cc433900c062c4
Linty <66986490+GreatFriend129@users.noreply.github.com>
2022-11-11 11:38:33 -0500

    Recovered Main method
    That was almost a disaster

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,130 @@
+import java.util.*;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        int choice;
+
+        do {
+
+            System.out.println("Welcome to The Marketplace!");
+
+            do { // initial prompt
+
+                System.out.printf("1. Login\n2. Signup\n3. Exit\n");
+                choice = scan.nextInt();
+                scan.nextLine();
+
+            } while (choice != 1 && choice != 2 && choice != 3);
+
+            int answer;
+
+            do {
+
+                if (choice == 1) { // user chooses to login
+
+                    String username;
+                    String password;
+
+                    System.out.printf("Login Menu\n********\n");
+                    System.out.println("Enter your username:");
+                    username = scan.nextLine();
+                    System.out.println("Enter your password:");
+                    password = scan.nextLine();
+
+                    User user = new User(username, password);
+
+                    if (!(user.login())) { // user login fails
+
+                        System.out.println("Your username or password is incorrect.");
+                        System.out.printf("1. Try again?\n2. Signup\n");
+
+                        do {
+                            answer = scan.nextInt();
+                            scan.nextLine();
+                        } while (answer != 1 && answer != 2);
+
+                        if (answer == 1) {
+                            choice = 1;
+                            continue;
+                        } else if (answer == 2) {
+                            choice = 2;
+                            continue;
+                        }
+
+                    } else { // user login is successful
+
+
+
+                    }
+
+                } else if (choice == 2) { // user chooses to signup
+
+                    String newUsername;
+                    String newPassword;
+                    String agentType = "";
+                    int sellOrBuy;
+                    User user = null;
+
+                    do {
+
+                        System.out.printf("Signup Menu\n********\n");
+                        System.out.println("Enter a username:");
+                        newUsername = scan.nextLine();
+                        System.out.println("Enter a strong password:");
+                        newPassword = scan.nextLine();
+
+                        do {
+                            System.out.printf("Are you a seller or buyer?\n1. Seller\n2. Buyer\n");
+                            sellOrBuy = scan.nextInt();
+                            scan.nextLine();
+                        } while (sellOrBuy != 1 && sellOrBuy != 2);
+
+                        if (sellOrBuy == 1) {
+                            agentType = "seller";
+                        } else if (sellOrBuy == 2) {
+                            agentType = "buyer";
+                        }
+
+                        user = new User(newUsername, newPassword, agentType);
+
+                        if (user.signUp()) {
+                            choice = 1;
+                            break;
+                        } else {
+
+                            System.out.println("User already exists!\n1. Login\n2. Signup\n3. Exit\n");
+                            int temp;
+                            do {
+                                temp = scan.nextInt();
+                            } while (temp != 1 && temp != 2 && temp != 3);
+
+                            if (temp == 1) {
+                                choice = 1;
+                                break;
+                            } else if (temp == 2) {
+                                choice = 2;
+                                break;
+                            } else {
+                                choice = 3;
+                                break;
+                            }
+
+                        }
+
+                    } while (true);
+
+                } else if (choice == 3) { // user chooses to exit
+                    System.out.println("Goodbye!");
+                    break;
+                }
+
+            } while (true);
+
+        } while (choice != 3);
+
+    }
 
 }
